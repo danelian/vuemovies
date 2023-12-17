42d83daa3d14f9ce2e785fac06b939df875b08f0
danelian <141317269+danelian@users.noreply.github.com>
2023-12-17 14:17:14 +0300

    pinia - state

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { useMovieStore } from './stores/MovieStore'
+import Movie from './components/Movie.vue'
+
+const movieStore = useMovieStore()
+</script>
+
+<template>
+  <main>
+    <header class="header">
+      <img src="/logo.svg" class="header-logo" alt="logo">
+      <h2>My Favorite Movies</h2>
+    </header>
+    <div class="tabs">
+      <button :class="['btn', {btn_green: movieStore.activeTab === 1}]">Favorite</button>
+      <button :class="['btn', {btn_green: movieStore.activeTab === 2}]">Search</button>
+    </div>
+    <div class="movies" v-if="movieStore.activeTab === 1">
+      <h3>All Movies</h3>
+      <Movie 
+        v-for="movie of movieStore.movies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+    <div class="search" v-if="movieStore.activeTab === 2">
+      <h3>Search</h3>
+    </div>
+  </main>
+</template>
+
+<style lang="css">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+.btn_green {
+  background: #37df5c;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
