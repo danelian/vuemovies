8dcf96da1e91ac1d848a52287dbe9c8f1cb36914
danelian <141317269+danelian@users.noreply.github.com>
2023-12-17 21:56:54 +0300

    pinia - composition api

--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,24 +1,27 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useMovieStore } from './MovieStore'
 const url = "http://www.omdbapi.com/?i=tt3896198&apikey=c372a2e0&s";
 
-export const useSearchStore = defineStore('searchStore', {
-  state: () => ({
-    loader: false,
-    movies: [],
-  }),
-  actions: {
-    async getMovies(search) {
-      this.loader = true
-      const res = await fetch(`${url}${search}`)
-      const data = await res.json();
-      this.movies = data.results;
-      this.loader = false; 
-    },
-    addToUseMovies(object) {
-      const movieStore = useMovieStore();
-      movieStore.movies.push({ ...object, isWatched: false });
-      movieStore.activeTab = 1;
-    }
+export const useSearchStore = defineStore('searchStore', () => {
+  const loader = ref(false)
+  const movies = ref([])
+
+  const getMovies = async(search) => {
+    loader.value = true
+    const res = await fetch(`${url}${search}`);
+    const data = await res.json();
+    movies.value = data.results;
+    loader.value = false
+  }
+
+  const addToUseMovies = (object) => {
+    const movieStore = useMovieStore();
+    movieStore.movies.push({ ...object, isWatched: false });
+    movieStore.activeTab = 1;
+  }
+
+  return {
+    loader, movies, getMovies, addToUseMovies
   }
 })
